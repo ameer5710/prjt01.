fd3b23c8f6fd9c9823f762e369afeb72f81a0814
Ameer Hamza <150230129+ameer5710@users.noreply.github.com>
2024-01-23 16:05:05 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-red-700">
+      <div className='text-black text-3xl bg-white'>HOME PAGE</div>
+    </main>
+  )
+}
